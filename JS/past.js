8f0e5f89c0d8f0f0ffd367832da05445114dde32
Einglitz <campagne.rodrigo@gmail.com>
2023-03-02 21:34:58 -0300

    Update past.js

--- JS/past.js
+++ JS/past.js
@@ -9,26 +9,26 @@ function printPastEvents(){
     if (card.date<data.currentDate){
         let listaEvents = 
         `<div class="event-card col h-100 tarjeta">
-        <div class="card shadow-sm">
-            <img src="${card.image}" alt="${card.name}" class="img-fit">
-            <h4 class="card-title bg-light bg-opacity-70 text-center">${card.name}</h4>
-            <div class="card-body">
-                <h6 class="card-subtitle small mb-4"><span class="text-muted">Category : </span><span class="strong">${card.category}</span></h6>
-                <p class="card-text">${card.description}</p>
-                
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">                                
-                        <button type="button" class="btn btn-sm btn-outline-info"><a href="details.html">View event</a></button>
+            <div class="card shadow-sm">
+                <img src="${card.image}" alt="${card.name}" class="img-fit">
+                <h4 class="card-title bg-light bg-opacity-70 text-center">${card.name}</h4>
+                <div class="card-body">
+                    <h6 class="card-subtitle small mb-4"><span class="text-muted">Category : </span><span class="strong">${card.category}</span></h6>
+                    <p class="card-text">${card.description}</p>
+                    
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">                                
+                            <button type="button" class="btn btn-sm btn-outline-info"><a href="details.html">View event</a></button>
+                        </div>
+                        <small class="text-muted">${card.price}</small>
                     </div>
-                    <small class="text-muted">${card.price}</small>
+                </div>
+
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 10 min ago</small>
                 </div>
             </div>
-
-            <div class="card-footer">
-                <small class="text-muted">Last updated 10 min ago</small>
-            </div>
-        </div>
-    </div>`
+        </div>`
         
             pastEvents.push(listaEvents);
             console.log(pastEvents);
